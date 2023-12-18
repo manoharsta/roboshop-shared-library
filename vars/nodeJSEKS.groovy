823bf6f3b03abaac947cdf71a1931cfcf3ee4970
manoharsta <manoharsta11@gmail.com>
2023-12-18 18:20:08 +0530

    shared libraries

--- vars/nodeJSEKS.groovy
+++ vars/nodeJSEKS.groovy
@@ -53,7 +53,7 @@ def call(Map configMap){
                     nexusArtifactUploader(
                         nexusVersion: 'nexus3',
                         protocol: 'http',
-                        nexusUrl: '172.31.86.20:8081/',
+                        nexusUrl: '18.212.126.157:8081/',
                         groupId: 'com.roboshop',
                         version: "$packageVersion",
                         repository: "${component}",
